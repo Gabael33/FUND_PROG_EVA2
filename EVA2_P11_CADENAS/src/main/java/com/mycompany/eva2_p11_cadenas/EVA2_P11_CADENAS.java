8c0c42fb6eba9d25bb1a644467e6e8ba5a9322b7
Gabael33 <143562822+Gabael33@users.noreply.github.com>
2023-10-18 17:52:22 -0600

    Add files via upload

---
+++ EVA2_P11_CADENAS/src/main/java/com/mycompany/eva2_p11_cadenas/EVA2_P11_CADENAS.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_p11_cadenas;
+
+/**
+ *
+ * @author Gabael
+ */
+public class EVA2_P11_CADENAS {
+
+    public static void main(String[] args) {
+        String cade = "HOLA";
+        
+        
+        if (cade.equals("HOLA"))
+            System.out.println("Las cadenas son iguales");
+        else
+            System.out.println("Las cadenas son diferentes");
+    }
+}
